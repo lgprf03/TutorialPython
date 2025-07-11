1b1154bf9166cf805a5809863b15d61d963e3ce1
lgprf03 <lg.prf03@gmail.com>
2025-07-11 16:32:27 -0500

    Mensaje de Inicio

--- 01_basico/08_clausulaTryCatch.py
+++
@@ -1,8 +0,0 @@
-if __name__ == "__main__":
-    # Excepciones
-    try:
-        num = int(input("Escriba un numero: "))
-    except: # Saltara a caso de que un proceso no se haya realizado como en el try
-        print("Lo escrito no es un numero")
-    finally: # Esto ocurrira ocurra o no la excepcion, se puede omitir
-        print("El numero se ha escrito")
--- 01_basico/ejemploImporte.py
+++
@@ -1,6 +0,0 @@
-def factorial(num, result=1): 
-    if num == 0: 
-        return result 
-    else: 
-        return factorial(num - 1, result * num)
-    
